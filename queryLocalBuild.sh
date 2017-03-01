a3f2ec251a3abef396d1a021240edadf99cf17a6
panzh <panzh@cisco.com>
2017-03-01 16:28:35 +0800

    test travis

--- queryLocalBuild.sh
+++ queryLocalBuild.sh
@@ -1,11 +1,11 @@
 GREP_RETURN_CODE=0
-for ((i=0; i<=7; i++)); do
+for ((i=0; i<=10; i++)); do
 	curl --silent ${TiggerAddress}/job/SDKTesterJenkins/lastBuild/api/json | grep result\":SUCCESS > /dev/null
 	if [ $GREP_RETURN_CODE -eq "1" ] ; then	
 		exit 0
-	elif [ $i -eq "6" ] ; then
+	elif [ $i -eq "9" ] ; then
 		exit 1
 	else
-		sleep 300
+		sleep 180
 	fi
 done
