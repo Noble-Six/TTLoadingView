dae8bc1e03d082097b6811e872172cc58b17180b
panzh <panzh@cisco.com>
2017-03-02 15:48:21 +0800

    test traivs

--- queryLocalBuild.sh
+++ queryLocalBuild.sh
@@ -1,11 +1,16 @@
 GREP_RETURN_CODE=0
-echo ${QueryAddress}
 for ((i=0; i<=20; i++)); do
 	curl --silent ${QueryAddress} | grep result\":\"SUCCESS\" > /dev/null
 	GREP_RETURN_CODE=$?
-	echo "return code : $GREP_RETURN_CODE"
+	echo "check success return code : $GREP_RETURN_CODE"
 	if [ $GREP_RETURN_CODE -eq "0" ] ; then	
 		exit 0
+
+	curl --silent ${QueryAddress} | grep result\":\"FAILURE\" > /dev/null
+	GREP_RETURN_CODE=$?
+	echo "check failure return code : $GREP_RETURN_CODE"
+	if [ $GREP_RETURN_CODE -eq "0" ] ; then	
+		exit 1
 	elif [ $i -eq "19" ] ; then
 		# echo $GREP_RETURN_CODE
 		exit 1
