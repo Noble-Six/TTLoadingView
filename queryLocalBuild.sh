296c7ee932c4338af3d71c68d00a71d13fdca53c
panzh <panzh@cisco.com>
2017-03-02 11:39:05 +0800

    test

--- queryLocalBuild.sh
+++ queryLocalBuild.sh
@@ -1,6 +1,6 @@
 GREP_RETURN_CODE=0
 for ((i=0; i<=20; i++)); do
-	curl --silent ${TiggerAddress}/job/SDKTesterJenkins/lastBuild/api/json | grep result\":SUCCESS > /dev/null
+	curl --silent ${QueryAddress} | grep result\":SUCCESS > /dev/null
 	if [ $GREP_RETURN_CODE -eq "1" ] ; then	
 		exit 0
 	elif [ $i -eq "19" ] ; then
