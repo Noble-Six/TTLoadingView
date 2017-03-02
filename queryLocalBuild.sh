d57ed696b999c3156893daa295baffb45e886659
panzh <panzh@cisco.com>
2017-03-02 15:21:43 +0800

    travis test

--- queryLocalBuild.sh
+++ queryLocalBuild.sh
@@ -1,9 +1,10 @@
 GREP_RETURN_CODE=0
 echo ${QueryAddress}
 for ((i=0; i<=20; i++)); do
-	curl --silent ${QueryAddress} | grep result\":SUCCESS > /dev/null
+	curl --silent ${QueryAddress} | grep result\":\"SUCCESS\" > /dev/null
 	GREP_RETURN_CODE=$?
-	if [ $GREP_RETURN_CODE -eq "1" ] ; then	
+	echo "return code : $GREP_RETURN_CODE"
+	if [ $GREP_RETURN_CODE -eq "0" ] ; then	
 		exit 0
 	elif [ $i -eq "19" ] ; then
 		# echo $GREP_RETURN_CODE
