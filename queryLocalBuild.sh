054f9290365990e7f7020680544ac5eacf346e65
panzh <panzh@cisco.com>
2017-03-03 16:25:18 +0800

    test trave ci

--- queryLocalBuild.sh
+++ queryLocalBuild.sh
@@ -2,20 +2,21 @@ GREP_RETURN_CODE=0
 for ((i=0; i<=20; i++)); do
 	curl --silent ${QueryAddress} | grep result\":\"SUCCESS\" > /dev/null
 	GREP_RETURN_CODE=$?
-	echo "check success return code : $GREP_RETURN_CODE"
-	if [ $GREP_RETURN_CODE -eq "0" ] ; then	
+	
+	if [ $GREP_RETURN_CODE -eq "0" ] ; then
+		echo "check local build result return : SUCCESS"	
 		exit 0
 	fi
 	curl --silent ${QueryAddress} | grep result\":\"FAILURE\" > /dev/null
 	GREP_RETURN_CODE=$?
-	echo "check failure return code : $GREP_RETURN_CODE"
 	if [ $GREP_RETURN_CODE -eq "0" ] ; then	
+		echo "check local build result return : FAILURE"
 		exit 1
 	elif [ $i -eq "19" ] ; then
-		# echo $GREP_RETURN_CODE
+		echo "check local build result return : TIMEOUT"
 		exit 1
 	else
-		echo "sleep"
+		echo "sleep 60sec"
 		sleep 60
 	fi
 done
