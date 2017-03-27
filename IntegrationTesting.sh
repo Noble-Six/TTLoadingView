b6d21748d8e4b3df2e12bd4994e1cc8a49235223
panzh <panzh@cisco.com>
2017-03-27 10:35:45 +0800

    test travis

--- IntegrationTesting.sh
+++ IntegrationTesting.sh
@@ -15,6 +15,21 @@ for ((i=0; i<=20; i++)); do
 		if $IsIntegrationTestingTask -eq "true" ; then
 		curl --silent ${QueryAddress} | grep -o '"totalCount[^,]*' >tmp.txt
 		TOTAL_COUNT=$(<tmp.txt)
+		curl --silent ${QueryAddress} | grep -o '"duration[^,]*' >tmp.txt
+		DURATION=$(cut -d ":" -f 2 tmp.txt)
+		TEST_MIN=$[DURATION / 60 / 1000]
+		TEST_SEC=$[DURATION /1000 % 60]
+		echo "$TOTAL_COUNT ,duration:$TEST_MIN min $TEST_SEC sec"
+		fi
+		exit 0
+	fi
+	curl --silent ${QueryAddress} | grep result\":\"FAILURE\" > /dev/null
+	GREP_RETURN_CODE=$?
+	if [ $GREP_RETURN_CODE -eq "0" ] ; then	
+		echo "Integration testing FAILURE !"
+		if $IsIntegrationTestingTask -eq "true" ; then
+		curl --silent ${QueryAddress} | grep -o '"totalCount[^,]*' >tmp.txt
+		TOTAL_COUNT=$(<tmp.txt)
 		curl --silent ${QueryAddress} | grep -o '"failCount[^,]*' >tmp.txt
 		FAIL_COUNT=$(<tmp.txt)
 		curl --silent ${QueryAddress} | grep -o '"duration[^,]*' >tmp.txt
@@ -23,12 +38,6 @@ for ((i=0; i<=20; i++)); do
 		TEST_SEC=$[DURATION /1000 % 60]
 		echo "$TOTAL_COUNT , $FAIL_COUNT ,duration:$TEST_MIN min $TEST_SEC sec"
 		fi
-		exit 0
-	fi
-	curl --silent ${QueryAddress} | grep result\":\"FAILURE\" > /dev/null
-	GREP_RETURN_CODE=$?
-	if [ $GREP_RETURN_CODE -eq "0" ] ; then	
-		echo "Integration testing FAILURE !"
 		exit 1
 	elif [ $i -eq "19" ] ; then
 		echo "Integration testing : TIMEOUT"
