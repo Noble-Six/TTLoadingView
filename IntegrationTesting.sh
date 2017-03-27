ae4984f352ec14d480045060db3ff9e8710c8c49
panzh <panzh@cisco.com>
2017-03-27 10:12:24 +0800

    test travis

--- IntegrationTesting.sh
+++ IntegrationTesting.sh
@@ -1,7 +1,7 @@
 
 if $IsIntegrationTestingTask -eq "true" ; then
 	echo "start Integration testing..."
-	#- curl -XPOST --silent --show-error --user ${TiggerUser}:${TiggerToken} ${TiggerAddress}
+	- curl -XPOST --silent --show-error --user ${TiggerUser}:${TiggerToken} ${TiggerAddress}
 fi
 
 GREP_RETURN_CODE=0
@@ -11,7 +11,18 @@ for ((i=0; i<=20; i++)); do
 	GREP_RETURN_CODE=$?
 	
 	if [ $GREP_RETURN_CODE -eq "0" ] ; then
-		echo "Integration testing SUCCESS !"	
+		echo "Integration testing SUCCESS !"
+		if $IsIntegrationTestingTask -eq "true" ; then
+		curl --silent ${QueryAddress} | grep -o '"totalCount[^,]*' >tmp.txt
+		TOTAL_COUNT=$(<tmp.txt)
+		curl --silent ${QueryAddress} | grep -o '"failCount[^,]*' >tmp.txt
+		FAIL_COUNT=$(<tmp.txt)
+		curl --silent ${QueryAddress} | grep -o '"duration[^,]*' >tmp.txt
+		DURATION=$(cut -d ":" -f 2 tmp.txt)
+		TEST_MIN=$[DURATION / 60 / 1000]
+		TEST_SEC=$[DURATION /1000 % 60]
+		echo "$TOTAL_COUNT , $FAIL_COUNT ,duration:$TEST_MIN min $TEST_SEC sec"
+		fi
 		exit 0
 	fi
 	curl --silent ${QueryAddress} | grep result\":\"FAILURE\" > /dev/null
