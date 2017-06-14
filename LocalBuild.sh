f549284c9b292a8354baa6069a9e4441e0ade615
panzh <panzh@cisco.com>
2017-06-14 16:16:10 +0800

    test

--- LocalBuild.sh
+++ LocalBuild.sh
@@ -1,29 +1,7 @@
 
 if $IsIntegrationTestingTask -eq "true" ; then
 	echo "start Integration testing..."
-	#- curl -XPOST --silent --show-error --user ${TiggerUser}:${TiggerToken} ${TiggerAddress}
+	curl -XPOST --silent --show-error --user panzh: fb7c5baed9140e057133a557d2af4674 https://d35f8f14.ngrok.io/job/SDKTesterJenkins/buildWithParameters
 fi
 
-GREP_RETURN_CODE=0
-sleep 30
-for ((i=0; i<=20; i++)); do
-	curl --silent ${QueryAddress} | grep result\":\"SUCCESS\" > /dev/null
-	GREP_RETURN_CODE=$?
-	
-	if [ $GREP_RETURN_CODE -eq "0" ] ; then
-		echo "Integration testing SUCCESS !"	
-		exit 0
-	fi
-	curl --silent ${QueryAddress} | grep result\":\"FAILURE\" > /dev/null
-	GREP_RETURN_CODE=$?
-	if [ $GREP_RETURN_CODE -eq "0" ] ; then	
-		echo "Integration testing FAILURE !"
-		exit 1
-	elif [ $i -eq "19" ] ; then
-		echo "Integration testing : TIMEOUT"
-		exit 1
-	else
-		echo "wait 60sec."
-		sleep 60
-	fi
-done
+echo "$TRAVIS_BRANCH"
