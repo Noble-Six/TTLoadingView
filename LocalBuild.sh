fc4cbddad69c4943d21173f5c67cd6723e7d22be
panzh <panzh@cisco.com>
2017-06-14 17:54:00 +0800

    test

--- LocalBuild.sh
+++ LocalBuild.sh
@@ -1,7 +1,7 @@
 
 if $IsIntegrationTestingTask -eq "true" ; then
 	echo "start Integration testing..."
-	curl -XPOST --silent --show-error --user panzh:fb7c5baed9140e057133a557d2af4674 https://d35f8f14.ngrok.io/job/SDKTesterJenkins/buildWithParameters
+	curl -XPOST --silent --show-error --user ${TiggerUser}:${TiggerToken} ${TiggerAddress}
 fi
 
 echo "$TRAVIS_BRANCH"
