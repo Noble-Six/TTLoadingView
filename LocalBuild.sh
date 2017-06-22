c25ededdfdc8a2a1768222dcd4c5aa3c9bf628fc
panzh <panzh@cisco.com>
2017-06-22 18:23:35 +0800

    test

--- LocalBuild.sh
+++ LocalBuild.sh
@@ -1,3 +1,7 @@
-sendemail -f "$Email" -t "$Email" -u "Jenkins build $TRAVIS_BRANCH" -m "pull request:$TRAVIS_PULL_REQUEST" -s smtp.gmail.com -o tls=yes -xu "$Email" -xp "$EmailPWD"
+if $IsIntegrationTestingTask -eq "true" ; then
+	echo "start Integration testing..."
+	sendemail -f "$Email" -t "$Email" -u "Jenkins build $TRAVIS_BRANCH" -m "pull request:$TRAVIS_PULL_REQUEST" -s smtp.gmail.com -o tls=yes -xu "$Email" -xp "$EmailPWD"
+fi
+
 
 
