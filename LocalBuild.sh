79a3c1396b4cd48853a8706df68a009d23a9c4c6
panzh <panzh@cisco.com>
2017-06-26 18:17:58 +0800

    test

--- LocalBuild.sh
+++ LocalBuild.sh
@@ -3,16 +3,16 @@ if $IsIntegrationTestingTask -eq "true" ; then
 	sendemail -f "$Email" -t "$Email" -u "Jenkins build by Travis" -m "BranchName=$TRAVIS_BRANCH\nBuildNumber=$TRAVIS_JOB_ID\nPullRequestNumber=$TRAVIS_PULL_REQUEST" -s smtp.gmail.com -o tls=yes -xu "$Email" -xp "$EmailPWD"
 fi
 
-sleep 60
+sleep 120
 
-for ((i=0; i<=20; i++)); do
-buildEmailTitle=`curl -u $username:$password --silent "https://mail.google.com/mail/feed/atom/important" | awk -F '<title>' '{for (i=3; i<=NF; i++) {print $i"\n"}}' | awk -F '</title>' '{for (i=1; i<=NF; i=i+2) {print $i"\n"}}' | awk -F: '/^BuildNumber:'"$TRAVIS_JOB_ID"'/'`
-
+for ((i=0; i<=10; i++)); do
+buildEmailTitle=`curl -u $Email:$EmailPWD --silent "https://mail.google.com/mail/feed/atom/important" | awk -F '<title>' '{for (i=3; i<=NF; i++) {print $i"\n"}}' | awk -F '</title>' '{for (i=1; i<=NF; i=i+2) {print $i"\n"}}' | awk -F: '/^BuildNumber:'"$TRAVIS_JOB_ID"'/'`
+echo "$buildEmailTitle"
 if [ "$buildEmailTitle" == "" ]; then
 	#wait
 	echo "wait 60 sec"
-	sleep 60
-elif [ $i -eq "19" ] ; then
+	sleep 30
+elif [ $i -eq "24" ] ; then
 	echo "Integration testing : TIMEOUT!"
 	exit 1
 else
