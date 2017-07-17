d11998194e45f4d979173e29d0b293d37befeb2b
Noble-Six <panzh@cisco.com>
2017-07-17 18:27:14 +0800

    Bugfix/1.2.0 (#1)
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test travis email
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * test email
    
    * Update .travis.yml
    
    * test
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * Update LocalBuild.sh
    
    * Update LocalBuild.sh

--- LocalBuild.sh
+++ LocalBuild.sh
@@ -1,29 +1,36 @@
-
+echo "BuildNumber=$TRAVIS_JOB_ID"
 if $IsIntegrationTestingTask -eq "true" ; then
 	echo "start Integration testing..."
-	- curl -XPOST --silent --show-error --user ${TiggerUser}:${TiggerToken} ${TiggerAddress}
+	#sendemail -f "$Email" -t "$Email" -u "Jenkins build by Travis" -m "BranchName=$TRAVIS_BRANCH\nBuildNumber=$TRAVIS_JOB_ID\nPullRequestNumber=$TRAVIS_PULL_REQUEST" -s smtp.gmail.com -o tls=yes -xu "$Email" -xp "$EmailPWD"
+else 
+        echo "not a integration test task return ok..."
+	exit 0
 fi
 
-GREP_RETURN_CODE=0
-sleep 30
-for ((i=0; i<=20; i++)); do
-	curl --silent ${QueryAddress} | grep result\":\"SUCCESS\" > /dev/null
-	GREP_RETURN_CODE=$?
-	
-	if [ $GREP_RETURN_CODE -eq "0" ] ; then
-		echo "Integration testing SUCCESS !"	
-		exit 0
-	fi
-	curl --silent ${QueryAddress} | grep result\":\"FAILURE\" > /dev/null
-	GREP_RETURN_CODE=$?
-	if [ $GREP_RETURN_CODE -eq "0" ] ; then	
-		echo "Integration testing FAILURE !"
-		exit 1
-	elif [ $i -eq "19" ] ; then
-		echo "Integration testing : TIMEOUT"
+sleep 120
+
+for ((i=0; i<=10; i++)); do
+buildEmailTitle=`curl -u $Email:$EmailPWD --silent "https://mail.google.com/mail/feed/atom/important" | awk -F '<title>' '{for (i=3; i<=NF; i++) {print $i"\n"}}' | awk -F '</title>' '{for (i=1; i<=NF; i=i+2) {print $i"\n"}}' | awk -F: '/^BuildNumber:'"$TRAVIS_JOB_ID"'/'`
+echo "$buildEmailTitle"
+if [ "$buildEmailTitle" == "" ]; then
+	#wait
+	echo "wait 60 sec"
+	sleep 30
+elif [ $i -eq "24" ] ; then
+	echo "Integration testing : TIMEOUT!"
+	exit 1
+else
+	buildResult=`echo | awk '{print test}' test="$buildEmailTitle" | awk -F: '/Fixed/||/Successful/'`
+	#echo "$buildResult"
+	if [ "$buildResult" == "" ]; then
+		#build failed
+		echo "$buildEmailTitle"
 		exit 1
 	else
-		echo "wait 60sec."
-		sleep 60
+		echo "Jenkins build Successful!"
+		exit 0
 	fi
+fi
 done
+
+
