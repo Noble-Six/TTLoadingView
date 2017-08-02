0e2743622a3c57e7c66a16e96c86fdc1d8e62ae9
panzh <panzh@cisco.com>
2017-08-02 15:00:47 +0800

    Update LocalBuild.sh

--- LocalBuild.sh
+++ LocalBuild.sh
@@ -3,7 +3,7 @@ if $IsIntegrationTestingTask -eq "true" ; then
 	echo "start Integration testing..."
 	sendemail -f "$Email" -t "$Email" -u "Jenkins build by Travis" -m "BranchName=$TRAVIS_BRANCH\nBuildNumber=$TRAVIS_JOB_ID\nPullRequestNumber=$TRAVIS_PULL_REQUEST" -s smtp.gmail.com -o tls=yes -xu "$Email" -xp "$EmailPWD"
 else 
-        echo "not a integration test task return ok..."
+        echo "not a integration test task return ok...1"
 	exit 0
 fi
 
