79bc9c0ffed8082c4a23afe4fa823c33860d83c5
panzh <panzh@cisco.com>
2017-08-03 10:18:23 +0800

    aaa
    aaa

--- Test/Test/AppDelegate.swift
+++ Test/Test/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var window11adfadf23: UIWindow?
+    var window11adadfadffadf23: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
