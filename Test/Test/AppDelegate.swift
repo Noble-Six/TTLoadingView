38d8e6e395d46e09e7749fea1ded1ac240d8adf0
panzh <panzh@cisco.com>
2017-08-03 16:18:19 +0800

    test1231111

--- Test/Test/AppDelegate.swift
+++ Test/Test/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent whn the applicatioasdfasdfasdfn is about to move from active to inactive st123123adfadfasate. This can occur for temporary adfadfinterruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Sent whn the applicatioasdfasdfasdfn is about to move from active to inactive st123123adfadfasate. This can occur fortemporary adfadfinterruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method toafsfs pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
